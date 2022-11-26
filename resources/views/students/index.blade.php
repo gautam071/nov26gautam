c2de417b2afbe28cff6ee9899ff768df6041c070
Gautam Rajesh <gautam.rajesh@coloredcow.in>
2022-11-26 11:18:26 +0530

    Updated the bootstrap CSS and JS files in the laravel

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,3 +1,19 @@
-<h1>
-    Hello
-</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+
+@endsection
